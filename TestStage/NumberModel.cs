b314edee0e4446f8c38e76a7f4217941e6914a39
bram <bram.stads@student.fontys.nl>
2019-11-17 21:31:54 +0100

    project

---
+++ TestStage/NumberModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestStage
+{
+    public class NumberModel
+    {
+        public int Key { get; set; }
+        public int Total { get; set; }
+
+        public NumberModel(int key, int total)
+        {
+            this.Key = key;
+            this.Total = total;
+        }
+    }
+}
---
+++ TestStage/Program.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestStage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hallo = string.Format("{0:e}", 1,2e2);
+            int ha = Convert.ToInt32(hallo);
+            Console.WriteLine(ha);
+        }
+
+
+        public void opdrachtFizzBuzz()
+        {
+            for (int i = 1; i < 31; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) Console.WriteLine("fizzbuzz");
+                else if (i % 3 == 0) Console.WriteLine("fizz");
+                else if (i % 5 == 0) Console.WriteLine("buzz");
+                else Console.WriteLine(i);
+            }
+        }
+
+        public static void Opdracht3()
+        {
+            IEnumerable<int> numberEnum = RandomNumbers();
+            var q = numberEnum.GroupBy(x => x)
+            .Select(g => new { Value = g.Key, Count = g.Count() })
+            .OrderByDescending(x => x.Value);
+
+            foreach (var x in q)
+            {
+                Console.WriteLine(x.Value + " : " + x.Count);
+            }
+        }
+
+        public static void Opdracht4()
+        {
+            IEnumerable<int> numberEnum = RandomNumbers();
+            var q = numberEnum.GroupBy(x => x)
+            .Select(g => new { Value = g.Key, Count = g.Count() })
+            .OrderByDescending(x => x.Count);
+            foreach (var x in q)
+            {
+                String numberJson = JsonConvert.SerializeObject(new NumberModel(x.Value, x.Count));
+                Console.WriteLine(numberJson);
+            }
+
+
+        }
+
+        public static void Opdracht5(int m, int n)
+        {
+            if (m <= 3 && n <= 9)
+            {
+                int value = Ackermanns(m, n);
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("this method wil not go further as m = 3 and n = 9. If numbers above this combination are used the program will crash");
+            }
+
+        }
+
+        private static IEnumerable<int> RandomNumbers(int seed = 123)
+        {
+            var random = new System.Random(seed);
+            for (int i = 0; i < 1000; i++)
+            {
+                yield return random.Next(25);
+            }
+        }
+
+        public static int Ackermanns(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (m > 0 && n == 0) return Ackermanns((m - 1), 1);
+            else if (m > 0 && n > 0) return Ackermanns((m - 1), Ackermanns(m, (n - 1)));
+            else
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+        }
+
+
+
+
+
+    }
+}
